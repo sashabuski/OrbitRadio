125b0313af3b26e83c99496947755013609f9595
Sasha <sashabuskin12@gmail.com>
2025-04-15 03:32:01 +1200

    cors troubleshooting

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 
+
+app.use(cors());
+app.options("*", cors());
 // Load JSON data
 let stations;
 try {
