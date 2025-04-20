851e8ce64b0ae7f8bbe47534510d60bf9bfa2289
Sasha <sashabuskin12@gmail.com>
2025-04-21 04:07:33 +1200

    https

--- public/index.js
+++ public/index.js
@@ -279,7 +279,7 @@ async function fetchStationsFromAPI(limit = 500000) {
     
     showLoadingOverlay();
     try {
-        const response = await fetch('http://orbitradio.onrender.com/'); 
+        const response = await fetch('https://orbitradio.onrender.com/'); 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
